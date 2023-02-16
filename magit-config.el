b1ef5631bf9a64816516d7c78dc26a8acf823cfe
max <schwanzuslongu23@proton.me>
2023-02-16 02:17:12 +0000

    some garbage

--- magit-config.el
+++ magit-config.el
@@ -1,3 +1,8 @@
 
+(add-hook 'magit-status-mode-hook
+		  (lambda ()
+			(define-key xah-fly-command-map (kbd "<space>") 'xah-fly-leader-key-map)
 
 (define-key xah-fly-leader-key-map (kbd "/") 'magit-status)
+
+ 
